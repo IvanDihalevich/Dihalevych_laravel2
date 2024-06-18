20ab2ccabdb311ff0c9a60968c47b64da9613356
Ivan Dihalevych <143941559+IvanDihalevich@users.noreply.github.com>
2024-06-19 01:31:16 +0300

    lab 4 bone

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,3 +11,15 @@ Route::resource('rest', RestTestController::class)->names('restTest');
 Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', PostController::class)->names('blog.posts');
 });
+//Адмінка
+$groupData = [
+    'namespace' => 'App\Http\Controllers\Blog\Admin',
+    'prefix' => 'admin/blog',
+];
+Route::group($groupData, function () {
+    //BlogCategory
+    $methods = ['index','edit','store','update','create',];
+    Route::resource('categories', CategoryController::class)
+        ->only($methods)
+        ->names('blog.admin.categories');
+});
